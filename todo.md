4c791a8370fe005197358f52d6be49eaac46a935
kmirecki-rbcpolska <kmirecki@rbcpolska.pl>
2026-06-22 20:49:26 +0200

    Limited time off request signer options to managers

--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 2. domyślnie urlop wypoczynkowy
 3. domyślnie twój kierownik. na liście wszyscy kierownicy
 4. zaimportowane faktury z wszystkimi pozycjami
-5. przetestować wszytkie sposoby podpisu dla pracownika dla kierownika
+5. przetestować wszytkie sposoby podpisu dla pracownika dla kierownika (gotowe czcionki, manualny, wgranie z dysku)
 6. własne pole do zamówienia w formie listy (przypisanie zamówieniom danej kategorii)
    1. szukamy centrów kosztowych (podział wydatków na kategorie i podkategorie eg. R&D -> labolatorium)
 7. limitacja widoczności faktur dla pracowników - jak tym sterować
--- wizards/hr_leave_sign_request_wizard.py
+++ wizards/hr_leave_sign_request_wizard.py
@@ -7,6 +7,9 @@ class HrLeaveSignRequestWizard(models.TransientModel):
     _description = "Create a sign request for a leave"
 
     leave_id = fields.Many2one("hr.leave", required=True, readonly=True)
+    available_manager_partner_ids = fields.Many2many(
+        "res.partner", compute="_compute_available_manager_partner_ids"
+    )
     template_id = fields.Many2one(
         "sign.oca.template",
         required=True,
@@ -16,13 +19,32 @@ class HrLeaveSignRequestWizard(models.TransientModel):
         "res.partner",
         string="Manager",
         required=True,
-        domain="[('user_ids', '!=', False)]",
+        domain="[('id', 'in', available_manager_partner_ids)]",
     )
+
+    @api.depends_context("uid")
+    def _compute_available_manager_partner_ids(self):
+        managers = self.env["hr.employee"].search(
+            [("leave_manager_id", "!=", False)]
+        ).mapped("leave_manager_id.partner_id")
+        for wizard in self:
+            wizard.available_manager_partner_ids = managers
 
     @api.model
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
         leave = self.env["hr.leave"].browse(self.env.context.get("default_leave_id"))
+        if leave and leave.holiday_status_id and not res.get("template_id"):
+            template = self.env["sign.oca.template"].search(
+                [
+                    ("model", "=", "hr.leave"),
+                    ("active", "=", True),
+                    ("time_off_type_id", "=", leave.holiday_status_id.id),
+                ],
+                limit=1,
+            )
+            if template:
+                res["template_id"] = template.id
         if leave and leave.employee_id and leave.employee_id.leave_manager_id:
             res.setdefault(
                 "manager_partner_id", leave.employee_id.leave_manager_id.partner_id.id
@@ -46,6 +68,10 @@ class HrLeaveSignRequestWizard(models.TransientModel):
         leave = self.leave_id.sudo()
         if leave.sign_request_id:
             raise UserError(_("This time off request already has a sign request."))
+        if self.manager_partner_id not in self.available_manager_partner_ids:
+            raise UserError(
+                _("Please choose a manager from the available manager list.")
+            )
         if self.template_id.time_off_type_id:
             leave.write({"holiday_status_id": self.template_id.time_off_type_id.id})
         request_vals = self.template_id._prepare_sign_oca_request_vals_from_record(
